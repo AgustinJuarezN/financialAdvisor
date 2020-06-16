a874dc970b43c9b4db2518033eb9d76fb4ff7fa5
Agustín Juárez <agustin.juarez@travelgenio.com>
2020-06-16 17:27:55 -0300

    unit tests

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,9 +1,55 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+import { createStore } from 'redux';
 import App from './App';
+import { Provider } from 'react-redux'
+import rootReducer from './../reducers';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("GIVEN a Redux store created from the rootReducer", () => {
+  let store;
+  beforeEach(() => {
+    store = createStore(rootReducer);
+  });
+
+  describe("WHEN this is passed to Root", () => {
+    let container, getAllByText;
+    beforeEach(() => {
+      ({ container, getAllByText  } = render(
+        <Provider store={store} >
+          <App />
+        </Provider>
+      ));
+    });
+
+    test('renders Financial Advisor title', () => {
+      expect(container).toHaveTextContent(/Financial Advisor/i);
+    });
+
+    test('render Buttons', () => {
+      const buttons = container.querySelector('.risk-button');
+      expect(buttons).toBeTruthy();
+    });
+
+    describe("Risk level 10 was clicked", () => {
+      beforeEach(() => {
+        fireEvent.click(getAllByText('10')[0]);
+      });
+
+      test("THEN show active row with color", () => {
+         const row = container.querySelector('.active-row');
+         expect(row).toBeTruthy();
+      });
+    });
+
+    describe("Show Donut button was clicked", () => {
+      beforeEach(() => {
+        fireEvent.click(container.querySelector('.button-mode'));
+      });
+
+      test("THEN show donut", () => {
+        const donut = container.querySelector('.donut');
+         expect(donut).toBeTruthy();
+      });
+    });
+  })
 });
